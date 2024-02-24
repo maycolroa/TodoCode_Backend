a982eb4dceaac18cae947cfd6dffe7182e01924c
Leonardo Daniel <leodaniel1978@gmail.com>
2024-02-24 12:16:53 -0300

    First commit

---
+++ src/main/java/com/hackacode/agenciaturistica/AgenciaturisticaApplication.java
@@ -0,0 +1,13 @@
+package com.hackacode.agenciaturistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaturisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaturisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackacode/agenciaturistica/AgenciaturisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackacode.agenciaturistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaturisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
